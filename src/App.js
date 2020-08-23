f14e9324f72a3c49ea950605288a74694e8c6f06
haveeru <suood_mohamed@hotmail.com>
2020-08-23 13:06:21 -0700

    add async await to axios

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import axios from "axios";
 import SearchBar from "./components/SearchBar";
 
 function App() {
-  const onSearchSubmit = (term) => {
-    axios.get("https://api.unsplash.com/search/photos", {
+  const onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID KZFILu2UG23wX_TdrhI3sjUImHtWdwhv5b8r0ssrwoE",
       },
     });
+    console.log(response.data.results)
   };
 
   return (
