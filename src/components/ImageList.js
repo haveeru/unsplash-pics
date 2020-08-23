ccd7737b08e911a878f8aa77c09dd5d5ead0cec9
haveeru <suood_mohamed@hotmail.com>
2020-08-23 14:05:07 -0700

    rendering images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 //props.images destructured
-const ImageList = ({images}) => {
-    console.log(images);
-    return (
-        <div>
-          ImageList  
-        </div>
-    );
+const ImageList = ({ images }) => {
+    console.log(images)
+  const imgUrls = images.map((image) => {
+    return <img src={image.urls.regular} alt={image.alt_description} key={image.id}/>;
+  });
+  return <div>{imgUrls}</div>;
 };
 
 export default ImageList;
