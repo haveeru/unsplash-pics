8023314d2eb4624c84b0f83a6a46814601633400
haveeru <suood_mohamed@hotmail.com>
2020-08-23 13:14:40 -0700

    useState hook to load data from axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const [images, setImages] = useState([])
+
   const onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
@@ -10,12 +12,13 @@ function App() {
         Authorization: "Client-ID KZFILu2UG23wX_TdrhI3sjUImHtWdwhv5b8r0ssrwoE",
       },
     });
-    console.log(response.data.results)
+    setImages(response.data.results)
   };
 
   return (
     <div className="ui container" style={{ marginTop: "10px" }}>
       <SearchBar onSubmit={onSearchSubmit} />
+      Found: {images.length} images
     </div>
   );
 }
