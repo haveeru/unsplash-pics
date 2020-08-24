66ada6bacf6abb82fdf37fe3bb3f078700d11b91
haveeru <suood_mohamed@hotmail.com>
2020-08-24 10:06:50 -0700

    get the image height using Ref

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // destruct urls and description form image props
 const ImageCard = ({ image: { urls, desription } }) => {
+  const imageRef = React.useRef();
+
+  // componentdidmount()
+  useEffect(() => {
+      //image is not loaded so it returns zero
+    console.log(`mount: ${imageRef.current.clientHeight}`);
+    // to solve the issue use an event listner
+    imageRef.current.addEventListener('load', setSpans)
+  }, []);
+
+  const setSpans = () => {
+    //now it will return the height of the image
+    console.log(imageRef.current.clientHeight);
+  }
+
   return (
     <div>
-      <img src={urls.regular} alt={desription} />
+      <img ref={imageRef} src={urls.regular} alt={desription} />
     </div>
   );
 };
